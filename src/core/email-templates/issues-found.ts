8c1284ca5abcac4e44cdbd481a7de7b32279c743
Jeff Mendez <jeffmendez19@gmail.com>
2021-04-09 01:39:19 -0400

    chore(email): add block level on email body issues

--- src/core/email-templates/issues-found.ts
+++ src/core/email-templates/issues-found.ts
@@ -22,33 +22,22 @@ const issuesFoundTemplate: IssuesFound = (
 
   if (data.issues?.length) {
     data.issues.some((item: Issue, i: number) => {
-      if (i > 4) {
+      if (i === 4) {
         return true
       }
-      listData = `${listData}
-      <tr>
-        <td>${item?.type}</td>
-        <td>${item?.context}</td>
-        <td>${item?.message}</td>
-      </tr>
-`
+      listData = `${listData}<tr><td>${item?.type}</td><td>${item?.context}</td><td>${item?.message}</td></tr>`
       return false
     })
   }
+
   const page = data?.pageUrl
-  return `
-  <h1>${data?.issues?.length} issues found for ${page}</h1>
-  <table>
-    <tr>
-      <th>Type</th>
-      <th>Message</th>
-      <th>Context</th>
-    </tr>
-    ${listData}
-  </table>
-  <a href="https://www.a11ywatch.com/dashboard" style="font-weight: 800; font-size: 1.8em; display: block; background: #5c6bc0; padding: 8px; color: white; text-align: center; text-decoration: none; margin-bottom: 10px;">View Details</a>
-  <a href="https://api.a11ywatch.com/api/get-website?q=${page}&download=true" style="font-weight: 800; font-size: 1.8em; display: block; background: #fff; padding: 8px; color: #ccc; text-align: center; text-decoration: none;">Download Report</a>
-  <p style="margin-top:10; margin-bottom: 10px;">If you want to stop receiving emails toggle the alert setting to off on the dashboard</p>
+  return `<div>
+    <h1>${data?.issues?.length} issues found for ${page}</h1>
+    <table><tr><th>Type</th><th>Message</th><th>Context</th></tr>${listData}</table>
+    <a href="https://www.a11ywatch.com/dashboard" style="font-weight: 800; font-size: 1.8em; display: block; background: #5c6bc0; padding: 8px; color: white; text-align: center; text-decoration: none; margin-bottom: 10px;">View Details</a>
+    <a href="https://api.a11ywatch.com/api/get-website?q=${page}&download=true" style="font-weight: 800; font-size: 1.8em; display: block; background: #fff; padding: 8px; color: #ccc; text-align: center; text-decoration: none;">Download Report</a>
+    <p style="margin-top:10; margin-bottom: 10px;">If you want to stop receiving emails toggle the alert setting to off on the dashboard</p>
+  </div>
 `
 }
 
