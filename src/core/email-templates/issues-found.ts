e1c28c399c4921fe76cc56a8780e4f4efff57f86
Jeff Mendez <jeffmendez19@gmail.com>
2021-12-19 18:43:10 -0500

    chore(email): update show errors on email report

--- src/core/email-templates/issues-found.ts
+++ src/core/email-templates/issues-found.ts
@@ -20,9 +20,12 @@ const issuesFoundTemplate: IssuesFound = (
 ) => {
   let listData = "";
   const tdStyles = `style="border: 1px solid #ddd; padding: 6px;"`;
+  const errorIssues = data?.issues?.length
+    ? data?.issues?.filter((iss) => iss?.type === "error")
+    : [];
 
-  if (data.issues?.length) {
-    data.issues.some((item: Issue, i: number) => {
+  if (errorIssues?.length) {
+    errorIssues.some((item: Issue, i: number) => {
       if (i === 10) {
         return true;
       }
@@ -42,6 +45,7 @@ const issuesFoundTemplate: IssuesFound = (
       </style>
     </head>
     <h1>${data?.issues?.length} issues found for ${page}</h1>
+    <div>Login to see full report</div>
     <div style="overflow-x:auto;">
     <table class="a11y-view" style="font-family: system-ui, Arial, Helvetica, sans-serif; border-collapse: collapse; width: 100%;">
       <tr>
@@ -52,7 +56,7 @@ const issuesFoundTemplate: IssuesFound = (
       ${listData}
     </table>
     </div>
-    <a href="https://a11ywatch.com/dashboard" style="font-weight: 800; font-size: 1.8em; display: block; background: #5c6bc0; padding: 8px; color: white; text-align: center; text-decoration: none;">View Details</a>
+    <a href="https://a11ywatch.com/dashboard" style="font-weight: 800; font-size: 1.8em; display: block; background: #5c6bc0; padding: 8px; color: white; text-align: center; text-decoration: none;">View Full Details</a>
     <a href="https://a11ywatch.com/reports/${page}" style="font-weight: 800; font-size: 1.8em; display: block; background: #111; padding: 8px; color: #fff; text-align: center; text-decoration: none;">Share Report</a>
     <a href="https://api.a11ywatch.com/api/get-website?q=${page}&download=true" style="font-weight: 800; font-size: 1.8em; display: block; background: #fff; padding: 8px; color: #ccc; text-align: center; text-decoration: none;">Download Report</a>
     <p style="margin-top:10px; margin-bottom: 10px;">If you want to stop receiving emails toggle the alert setting to off on the dashboard</p>
