4a1c7c54758bc31bae882c75d0a7b4ddf06da900
j-mendez <jeff@a11ywatch.com>
2022-03-13 12:50:46 -0400

    chore(fix): proxy

--- src/rest/routes/services/proxy-cdn.ts
+++ src/rest/routes/services/proxy-cdn.ts
@@ -1,16 +1,4 @@
-import url from "url";
 import proxy from "express-http-proxy";
 
 // move to cdn env var
-export const cdnProxy = proxy("http://cdn-server:8090", {
-  proxyReqPathResolver: (req) => {
-    const path = url.parse(req.originalUrl).path;
-
-    // remove target path since production uses another cdn
-    if (!process.env.PROXY_PRESERVE_CDN) {
-      return path.replace("/cdn", "");
-    }
-
-    return path;
-  },
-});
+export const cdnProxy = proxy("http://cdn-server:8090");
