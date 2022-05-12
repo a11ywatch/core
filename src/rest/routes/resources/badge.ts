f60fb2dfb10a4c1c5f0f331a2d14482d563f78bf
j-mendez <jeff@a11ywatch.com>
2022-05-12 10:08:05 -0400

    chore(badges): add default etag handling

--- src/rest/routes/resources/badge.ts
+++ src/rest/routes/resources/badge.ts
@@ -1,6 +1,5 @@
 import { rawStatusBadge } from "@app/core/assets";
 import { AnalyticsController } from "@app/core/controllers";
-import { hashString } from "@app/core/utils";
 import { Analytic } from "@app/types";
 import type { Request, Response } from "express";
 
@@ -28,9 +27,6 @@ export const statusBadge = async (req: Request, res: Response) => {
     }
   }
 
-  const etagHash = hashString(score);
-
   res.setHeader("Content-Type", "image/svg+xml");
-  res.setHeader("ETag", etagHash);
   res.send(rawStatusBadge({ statusColor, score }));
 };
