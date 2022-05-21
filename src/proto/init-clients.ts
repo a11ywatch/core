31e2a4b68923418ea117ae8eccf4b5a12e3158ea
j-mendez <jeff@a11ywatch.com>
2022-05-21 11:17:50 -0400

    chore(gRPC): add slight client connection delays on startup

--- src/proto/init-clients.ts
+++ src/proto/init-clients.ts
@@ -5,11 +5,27 @@ import {
   createCrawlerClient,
 } from "./website-client";
 
-export const startClientsGRPC = async () => {
+export const startClientsGRPC = async (retry?: boolean) => {
   // prevent outside startup for now
-  if (process.env.NODE_ENV !== "test") {
-    await createPageMindClient();
-    await createCrawlerClient();
-    await createMavClient();
-  }
+  return new Promise(async (resolve) => {
+    if (process.env.NODE_ENV !== "test") {
+      setTimeout(async () => {
+        try {
+          await createPageMindClient();
+          await createCrawlerClient();
+          await createMavClient();
+          console.log("gRPC clients connected - pagemind, crawler, and mav.");
+        } catch (e) {
+          console.error(e);
+          // try connection assume clients are all connected. TODO: remove HTTP health checks for gRPC.
+          if (!retry) {
+            return await startClientsGRPC(true);
+          }
+        }
+
+        resolve(true);
+      }, 60);
+    }
+    resolve(true);
+  });
 };
