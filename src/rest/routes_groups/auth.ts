a98b10f144c7817c15d96b9d05ab8d6418b65588
j-mendez <jeff@a11ywatch.com>
2022-06-10 09:53:11 -0400

    chore(gh): add gh login callback endpoint

--- src/rest/routes_groups/auth.ts
+++ src/rest/routes_groups/auth.ts
@@ -3,8 +3,51 @@ import cors from "cors";
 import { createUser } from "@app/core/controllers/users/set";
 import { verifyUser } from "@app/core/controllers/users/update";
 import { getUserFromToken } from "@app/core/utils";
-import { cookieConfigs } from "@app/config";
+import { config, cookieConfigs } from "@app/config";
 import { getUser } from "@app/core/controllers/users";
+import { request } from "https";
+
+const clientID = process.env.GITHUB_CLIENT_ID;
+const clientSecret = process.env.GITHUB_CLIENT_SECRET;
+
+// Authenticate with github @param requestToken string
+const oAuthGithub = (requestToken: string): Promise<any> => {
+  let rawData = "";
+
+  return new Promise((resolve, reject) => {
+    const req = request(
+      {
+        method: "POST",
+        hostname: "https://github.com",
+        port: 443,
+        headers: {
+          accept: "application/json",
+        },
+        path: `/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
+      },
+      (r) => {
+        r.setEncoding("utf8");
+
+        r.on("data", (chunk) => {
+          rawData += chunk;
+        });
+      }
+    );
+    req.on("error", (err) => {
+      reject(err);
+    });
+    req.on("end", () => {
+      let data;
+      try {
+        data = JSON.parse(rawData);
+      } catch (e) {
+        console.error(e);
+      }
+
+      resolve(data);
+    });
+  });
+};
 
 export const setAuthRoutes = (app: Application) => {
   app.post("/api/register", cors(), async (req, res) => {
@@ -69,5 +112,20 @@ export const setAuthRoutes = (app: Application) => {
       // un-authed user
       res.send(true);
     }
+  });
+
+  // [WIP] Github Authentication
+  app.get("/github/callback", async (req, res) => {
+    const requestToken = req.query.code + "";
+    const authentication = await oAuthGithub(requestToken);
+    const accessToken = authentication?.data?.access_token;
+    // TODO: perform auth and set to cookie. (Needs to redirect to add gql param to perform auth mutation)
+    console.log(authentication, accessToken);
+
+    res.redirect(
+      authentication
+        ? `${config.DOMAIN}/auth-redirect?access_token=${accessToken}`
+        : `${config.DOMAIN}`
+    );
   });
 };
