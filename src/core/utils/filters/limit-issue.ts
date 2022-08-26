e7f13fe9a640ca9e2749de53fb8ced9a0854ac65
j-mendez <jeff@a11ywatch.com>
2022-08-26 07:16:07 -0400

    chore(api): fix issue limiter

--- src/core/utils/filters/limit-issue.ts
+++ src/core/utils/filters/limit-issue.ts
@@ -1,10 +1,22 @@
 import type { Issue } from "../../../types/types";
 
 // limit the issue records to 1/4th
+// @return Issue['issues'][]
 export const limitIssue = (issues: Issue) => {
-  return (
-    issues?.issues?.slice(
-      issues?.issues.length - Math.max(Math.round(issues?.issues.length / 4), 2)
-    ) || []
+  const list =
+    issues?.issues && Array.isArray(issues.issues) ? [...issues?.issues] : [];
+  const half = Math.ceil(list.length / 2);
+
+  const tlist = list.slice(0, half);
+  const blist = list.slice(half);
+
+  const top = tlist.slice(
+    tlist.length - Math.max(Math.round(tlist.length / 4), 2)
   );
+
+  const bottom = blist.slice(
+    blist.length - Math.max(Math.round(blist.length / 4), 2)
+  );
+
+  return [...top, bottom];
 };
