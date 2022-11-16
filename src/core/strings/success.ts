886a7185429f9da14b594a22caad27b5b90a1eba
j-mendez <jeff@a11ywatch.com>
2022-11-16 12:13:09 -0500

    chore(q): add safe q threshold

--- src/core/strings/success.ts
+++ src/core/strings/success.ts
@@ -2,4 +2,5 @@ export const SUCCESS = "Success";
 export const PASSWORD_RESET =
   "Please check your email for password reset code.";
 export const SUCCESS_DELETED_ALL = "Success, all websites deleted.";
-export const CRAWLER_FINISHED = "CRAWLER FINISHED SCAN";
+export const CRAWLER_FINISHED = "Scan complete";
+export const CRAWLER_COMMENCED = "Scan started";
