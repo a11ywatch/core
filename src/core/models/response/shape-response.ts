03637f1bc1543b0f5a2b59df2dd99f1d8a524279
j-mendez <jeff@a11ywatch.com>
2023-04-26 20:12:16 -0400

    chore(api): fix api message response shape

--- src/core/models/response/shape-response.ts
+++ src/core/models/response/shape-response.ts
@@ -3,7 +3,7 @@ import { ResponseParamsModel, ResponseModel } from "./types";
 
 // response model based on exact params
 export const shapeResponse = (params?: ResponseParamsModel): ResponseModel => {
-  let { success = true, message, code = StatusCode.Ok, data } = params ?? {};
+  let { success = true, message = "", code = StatusCode.Ok, data } = params ?? {};
 
   // determine success on code
   if (code >= StatusCode.BadRequest) {
